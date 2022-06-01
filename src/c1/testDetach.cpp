584381f4ce8f94e39081e07c207344d3f7befb8e
mingtiancai <2427373908@qq.com>
2022-06-01 20:57:21 +0800

    ENH: 测试一下多线程调试环境是否可用

--- src/c1/testDetach.cpp
+++ src/c1/testDetach.cpp
@@ -3,19 +3,30 @@
 
 using namespace std;
 
-void workerFunc()
+void workerFunc1()
 {
     while (1)
     {
         this_thread::sleep_for(2s);
-        cout << "workerFunc" << endl;
+        cout << "workerFunc1" << endl;
+    }
+}
+
+void workerFunc2()
+{
+    while (1)
+    {
+        this_thread::sleep_for(2s);
+        cout << "workerFunc2" << endl;
     }
 }
 
 int main(int argc, char **argv)
 {
-    thread t(workerFunc);
-    t.detach();
+    thread t1(workerFunc1);
+    thread t2(workerFunc2);
+    t1.join();
+    t2.join();
 
     cout << "end" << endl;
 }
