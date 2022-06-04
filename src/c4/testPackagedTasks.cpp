76a831efd8024036b69450515cd2e5dbc91d323b
mingtiancai <2427373908@qq.com>
2022-06-04 23:48:51 +0800

    update

--- src/c4/testPackagedTasks.cpp
+++ src/c4/testPackagedTasks.cpp
@@ -92,6 +92,10 @@ int main(int argc, char **argv)
 
     str = "222";
     demo.postTask<void(string)>(func1, ref(str));
+
+    this_thread::sleep_for(chrono::seconds(15));
+    demo.setShutDown();
+
     t1.join();
 
     return 0;
