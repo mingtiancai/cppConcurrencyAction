88182b5163aa8626b5669aad8c9231b7e05730c0
mingtiancai <2427373908@qq.com>
2022-06-04 19:21:37 +0800

    add testFuture.cpp

---
+++ src/c4/testFuture.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <chrono>
+
+using namespace std;
+
+int doSomething()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "do something: " << i << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+
+    return 1;
+}
+
+int main(int agrc, char **argv)
+{
+    future<int> res = async(doSomething);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "main: " << i << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+
+    cout << "res: " << res.get() << endl;
+
+    return 0;
+}
